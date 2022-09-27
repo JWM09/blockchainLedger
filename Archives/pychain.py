b97149ad2567ff54531fbf03d4dabc3b0f628d54
JWM09 <jonm5214@hotmail.com>
2022-09-27 09:09:35 -1000

    final upload

--- Archives/pychain.py
+++ Archives/pychain.py
@@ -49,7 +49,11 @@ import hashlib
 # @TODO
 # Create a Record Data Class that consists of the `sender`, `receiver`, and
 # `amount` attributes
-# YOUR CODE HERE
+@dataclass
+class Record:
+    sender: str
+    reciever: str
+    amount: float
 
 
 ################################################################################
@@ -68,9 +72,9 @@ class Block:
 
     # @TODO
     # Rename the `data` attribute to `record`, and set the data type to `Record`
-    data: Any
+    record: Record
 
-    creator_id: int
+    creator_id: int 
     prev_hash: str = "0"
     timestamp: str = datetime.datetime.utcnow().strftime("%H:%M:%S")
     nonce: int = 0
@@ -166,19 +170,19 @@ pychain = setup()
 
 # @TODO:
 # Delete the `input_data` variable from the Streamlit interface.
-input_data = st.text_input("Block Data")
+# input_data = st.text_input("Block Data") - IS IT REALLY AS EASY AS DELETING THIS LINE?
 
 # @TODO:
 # Add an input area where you can get a value for `sender` from the user.
-# YOUR CODE HERE
+sender = st.text_input('Enter Sender Info')
 
 # @TODO:
 # Add an input area where you can get a value for `receiver` from the user.
-# YOUR CODE HERE
+receiver = st.text_input('Enter Recipent Info')
 
 # @TODO:
 # Add an input area where you can get a value for `amount` from the user.
-# YOUR CODE HERE
+amount = st.text_input('Enter amount being sent')
 
 if st.button("Add Block"):
     prev_block = pychain.chain[-1]
@@ -189,8 +193,9 @@ if st.button("Add Block"):
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
-        data=input_data,
-        creator_id=42,
+        # data=input_data, - IN LIEU OF DELETING AT THIS TIME
+        record=Record(sender, receiver, amount),
+        creator_id=42, 
         prev_hash=prev_block_hash
     )
 
--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # blockchainLedger
 UW Fintech Bootcamp Module 18 Challenge - Build a Blockchain Ledger
+
+---
+
+## Technologies
+
+The following libraries are used in this analysis:  
+  
+  * streamlit  
+  * dataclasses
+  * typing
+  * datetime
+  * haslib
+  * pandas
+  
+---
+
+## Purpose of Program
+
+The main purpose of this project is to demonstrate an ability to work with the pandas environment to create a pychain that records blocks, adds data to the blocks and allows for review of the data included within the block.  Streamlit is the software package used to demonstrate this skill, and the code was developed in visualstudio.    
+  
+---
+
+## Examples
+
+Through the use of streamlit I was able to capute key key data including the data entry: 
+ 
+ ![Data Entry](Images/pychainDataEntry.png)
+ 
+ 
+Additionally I was able to build a ledger :
+  
+  ![Ledger](Images/pychainLedger.png)
+  
+And finally I developed a Block Inspector to verify the data included in the block:
+  
+  ![Block Inspector](Images/pychainBlockInspector.png)
+
+Overall I can certainly see the value of developing these types of skills as they are certainly part of the future of fintech.  
+
+---
+
+## Contributors
+
+The majority of this project has been done independently, with support from my tutor, Katy Kirby
+
+---
+
+## License
+
+Program is free to use without license.  Only request is that you notify author of use and application.
+  
+To discuss usage or general inquires please contact the author at jonm5214@gmail.com
